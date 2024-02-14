f2a323e34fd49d306a6916317a9f078bef0c741f
Przemysław Kudzia <p.kudzia10@gmail.com>
2024-02-14 11:50:37 +0100

    chore: initial commit

---
+++ src/main/java/pl/atipera/githubbrowser/GitHubBrowserApplication.java
@@ -0,0 +1,13 @@
+package pl.atipera.githubbrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubBrowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/atipera/githubbrowser/GitHubBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.atipera.githubbrowser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
