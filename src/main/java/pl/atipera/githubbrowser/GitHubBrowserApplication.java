7c145259c1ca9918023133c1fedd7133a31886fd
Przemysław Kudzia <p.kudzia10@gmail.com>
2024-02-14 12:10:29 +0100

    refactor: set up project

--- src/main/java/pl/atipera/githubbrowser/GitHubBrowserApplication.java
+++ src/main/java/pl/atipera/githubbrowser/GitHubBrowserApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitHubBrowserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitHubBrowserApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubBrowserApplication.class, args);
+    }
 }
