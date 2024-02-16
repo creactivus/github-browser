403f5b9d3d424c80d219f243a9014cc6486a5320
Przemysław Kudzia <p.kudzia10@gmail.com>
2024-02-16 14:12:45 +0100

    refactor: update models

--- src/main/java/pl/atipera/githubbrowser/model/Branch.java
+++ src/main/java/pl/atipera/githubbrowser/model/Branch.java
@@ -1,20 +1,30 @@
 package pl.atipera.githubbrowser.model;
 
-import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Branch {
+    @JsonProperty
     private String name;
-    private List<Commit> commits;
+    private String lastCommit;
+
+    @JsonProperty("commit")
+    private void getLastCommitSha(Map<String, String> commit) {
+        lastCommit = commit.get("sha");
+    }
 }
--- src/main/java/pl/atipera/githubbrowser/model/Repository.java
+++ src/main/java/pl/atipera/githubbrowser/model/Repository.java
@@ -1,21 +1,32 @@
 package pl.atipera.githubbrowser.model;
 
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Repository {
+    @JsonProperty
     private String name;
     private String owner;
     private List<Branch> branches;
+    // private boolean fork
+
+    @JsonProperty("owner")
+    private void getOwnerLogin(Map<String, String> owner) {
+        this.owner = owner.get("login");
+    }
 }
